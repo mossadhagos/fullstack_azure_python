e5a79e504456bb7913d0c07f9a2a7d25e4d69166
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2026-09-09 21:22:43 +0200

    feat: data processing and constants

---
+++ backend/src/backend/data_processing.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from backend.constants import DATA_PATH
+
+# load data file
+df = pd.read_csv(DATA_PATH)
+
+# cleaning missing value
+duration_columns = [
+    "Penumbral Eclipse Duration (m)",
+    "Partial Eclipse Duration (m)",
+    "Total Eclipse Duration (m)",
+]
+
+df[duration_columns] = df[duration_columns].replace("-", pd.NA)
+df["Eclipse Type"] = df["Eclipse Type"].fillna("Unknown")
+
+# create fiealds for the API to filter with
+df["Year"] = df["Calendar Date"].str.extract(r"^(-?\d+)").astype(int)
+
+df["category"] = df["Eclipse Type"].map({
+    "N": "Penumbral",
+    "P": "Partial",
+    "T": "Total",
+    "T+": "Total",
+}).fillna("Unknown")
+
+
+def to_records(dataframe: pd.DataFrame) -> list[dict]:
+    return dataframe.where(pd.notnull(dataframe),None).to_dict(orient="records")
+
+def summary(dataframe: pd.DataFrame) -> dict:
+    return {
+        "total_eclipse": len(dataframe),
+        "categories": dataframe["category"].value_counts().to_dict(),
+        "year_from": int(dataframe["year"].min()),
+        "year_to": int(dataframe["year"].max()),
+        "saros_series": int(dataframe["Saros Number"].nunique()),
+    }
